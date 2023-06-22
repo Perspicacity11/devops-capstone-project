9b1f21f36872066429b5ecad9e7acdf622800d3d
Perspicacity11 <{cassius.cr.naylor@gmail.com}>
2023-06-22 07:11:04 -0400

    Added update function and test

--- service/routes.py
+++ service/routes.py
@@ -95,15 +95,25 @@ def get_accounts(account_id):
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
 
-# ... place you code here to UPDATE an account ...
-
+@app.route("/accounts/<int:account_id>", methods=["PUT"])
+def update_accounts(account_id):
+    """
+    Update an account
+    This endpoint will update an account based on the posted data
+    """
+    app.logger.info("Request to update an account with ID: %s", account_id)
+    account = Account.find(account_id)
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND, f"Could not find account with ID [{account_id}]")
+    account.deserialize(request.get_json())
+    account.update()
+    return account.serialize(), status.HTTP_200_OK
 
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
 
 # ... place you code here to DELETE an account ...
-
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
